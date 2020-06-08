e8b3c9bd9ea7ec5e8ff98ebc332b47611f707f7a
fagnerlc <fagnerlc@outlook.com>
2020-06-07 22:55:01 -0300

    aula 98

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:expenses/components/transaction_user.dart';
 import 'package:flutter/material.dart';
+
 main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
+    return MaterialApp(
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
-
 
 class MyHomePage extends StatelessWidget {
   @override
@@ -15,10 +19,33 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Despesas Pessoais'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          )
+        ],
       ),
-      body: Center(
-        child: Text('Versão Inicial!'),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Gráfico'),
+                elevation: 5,
+              ),
+            ),
+            TransactionUser(),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
