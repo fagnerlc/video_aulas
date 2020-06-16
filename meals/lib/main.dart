13f2b066fc016d413441994fc1edd4896f970ea0
fagnerlc <fagnerlc@outlook.com>
2020-06-16 00:52:55 -0300

    teste3

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:meals/models/category.dart';
+//import 'package:meals/models/category.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'screens/categories_screen.dart';
 import 'utils/app_routes.dart';
