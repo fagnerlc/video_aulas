75f87b01f84b3b7aba3fb6ac423d92f7f8cbd5f8
falc <fagnerlc@outlook.com>
2020-06-17 18:08:24 -0300

    Projeto Shop inicial

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:shop/providers/products.dart';
+import 'package:shop/utils/app_routes.dart';
+import 'package:shop/views/product_detail_screen.dart';
+import './views/products_overview_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Minha Loja',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
+    return ChangeNotifierProvider(
+      create: (_) => Products(),
+          child: MaterialApp(
+          title: 'Minha Loja',
+          theme: ThemeData(
+      primarySwatch: Colors.brown,
+      accentColor: Colors.deepOrangeAccent,
+      fontFamily: 'Lato',
+          ),
+          home: ProductOverviewScreen(),
+          routes: {
+      AppRoutes.PRODUCT_DETAIL: (ctx) => ProductDetailScreen(),
+          },
+        ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Minha Loja')),
-      ),
-      body: Center(
-        child: Text('Vamos desenvolver uma loja?'),
-      ),
-    );
-  }
-}
+
--- shop/lib/views/products_overview_screen.dart
+++ shop/lib/views/products_overview_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/products.dart';
+import '../widgets/product_item.dart';
+import '../providers/products.dart';
 
 class ProductOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.items;
+
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Minha loja')),
+      ),
+      body: GridView.builder(
+        padding: EdgeInsets.all(10),
+        itemCount: products.length,
+        itemBuilder: (ctx, idx) => ProductItem(products[idx]),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
     );
   }
 }
