f958e6a1fa53416dec0f0a8a2239a0b1b3a20e37
Fagner Carvalho <fagnerlc@outlook.com>
2021-05-18 16:34:30 -0300

    ensure RemoteAuthentication returns an Account on 200

--- fordev/test/data/usecases/remote_authentication_test.dart
+++ fordev/test/data/usecases/remote_authentication_test.dart
@@ -24,6 +24,15 @@ void main() {
         email: faker.internet.email(), secret: faker.internet.password());
   });
   test('Should call HttpClient with correct URL', () async {
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenAnswer((_) async => {
+              'accessToken': faker.guid.guid(),
+              'name': faker.person.name(),
+            });
+
     await sut.auth(params);
 
     verify(httpClient.request(
@@ -91,5 +100,21 @@ void main() {
     final future = sut.auth(params);
 
     expect(future, throwsA(DomainError.invalidCredencials));
+  });
+
+  test('Should return an Account if HttpClient returns 200', () async {
+    final accessToken = faker.guid.guid();
+    when(httpClient.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenAnswer((_) async => {
+              'accessToken': accessToken,
+              'name': faker.person.name(),
+            });
+
+    final account = await sut.auth(params);
+
+    expect(account.token, accessToken);
   });
 }
