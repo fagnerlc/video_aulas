6e60359a681672dc66cdfbe0dca7625c5c3c325b
Fagner Carvalho <fagnerlc@outlook.com>
2021-06-11 18:51:57 -0300

    ensure HttpAdapter calls post without body

--- fordev/features/http/http_responses.md
+++ fordev/features/http/http_responses.md
@@ -3,7 +3,7 @@
 > ## Sucesso
 1. :white_check_mark: Request com verbo http válido (post)
 2. :white_check_mark: Passar nos headers o content type JSON
-3. :: Chamar request com body correto
+3. :white_check_mark: Chamar request com body correto
 4. :: Ok - 200 e resposta com dados
 5. :: No content - 204 e resposta sem dados
 
--- fordev/test/infra/http/http_adapter_test.dart
+++ fordev/test/infra/http/http_adapter_test.dart
@@ -19,7 +19,8 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(Uri.parse(url), headers: headers, body: jsonEncode(body));
+    final jsonBody = body != null ? jsonEncode(body) : null;
+    await client.post(Uri.parse(url), headers: headers, body: jsonBody);
   }
 }
 
@@ -44,6 +45,14 @@ void main() {
             'accept': 'application/json',
           },
           body: '{"any_key":"any_value"}'));
+    });
+
+    test('Should call post without body', () async {
+      await sut.request(url: url, method: 'post');
+      verify(client.post(
+        any,
+        headers: anyNamed('headers'),
+      ));
     });
   });
 }
