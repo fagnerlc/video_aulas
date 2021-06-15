85c66e8ac6d41d81b5bb2bf05acfc96f1e681d5e
Fagner Carvalho <fagnerlc@outlook.com>
2021-06-15 13:57:40 -0300

    ensure LoginPage loads with correct initial state

---
+++ fordev/test/ui/pages/login_page_test.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:fordev/ui/pages/pages.dart';
+
+void main() {
+  testWidgets('Should load with correct initial state',
+      (WidgetTester tester) async {
+    final loginPage = MaterialApp(home: LoginPage());
+    await tester.pumpWidget(loginPage);
+
+    final emailTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Email'),
+      matching: find.byType(Text),
+    );
+    expect(
+      emailTextChildren,
+      findsOneWidget,
+      reason:
+          'quando o textFormField tiver apenas um filho ele quer dizer quen não tem erros.',
+    );
+
+    final passwordTextChildren = find.descendant(
+      of: find.bySemanticsLabel('Senha'),
+      matching: find.byType(Text),
+    );
+    expect(
+      passwordTextChildren,
+      findsOneWidget,
+      reason:
+          'quando o textFormField tiver apenas um filho ele quer dizer quen não tem erros.',
+    );
+
+    final button = tester.widget<ElevatedButton>(find.byType(ElevatedButton));
+    expect(button.onPressed, null);
+  });
+}
