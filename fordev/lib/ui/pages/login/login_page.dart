6f238469a11d7de5c92801ba9e154d7f51d81daf
Fagner Carvalho <fagnerlc@outlook.com>
2021-06-17 18:50:40 -0300

    ensure LoginPage present loading

--- fordev/lib/ui/pages/login/login_page.dart
+++ fordev/lib/ui/pages/login/login_page.dart
@@ -11,83 +11,112 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            LoginHeader(),
-            Headline1(
-              text: 'login',
-            ),
-            Padding(
-              padding: const EdgeInsets.all(32.0),
-              child: Form(
-                child: Column(
+      body: Builder(
+        builder: (context) {
+          presenter.isLoadingStream.listen((isLoading) {
+            if (isLoading) {
+              showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (ctx) => SimpleDialog(
                   children: [
-                    StreamBuilder<String>(
-                        stream: presenter.emailErrorStream,
-                        builder: (context, snapshot) {
-                          return TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Email',
-                              icon: Icon(
-                                Icons.email,
-                                color: Theme.of(context).primaryColorLight,
-                              ),
-                              errorText: snapshot.data?.isEmpty == true
-                                  ? null
-                                  : snapshot.data,
-                            ),
-                            keyboardType: TextInputType.emailAddress,
-                            onChanged: presenter.validateEmail,
-                          );
-                        }),
-                    Padding(
-                      padding: const EdgeInsets.only(
-                        top: 8.0,
-                        bottom: 32,
-                      ),
-                      child: StreamBuilder<String>(
-                          stream: presenter.passwordErrorStream,
-                          builder: (context, snapshot) {
-                            return TextFormField(
-                              decoration: InputDecoration(
-                                labelText: 'Senha',
-                                icon: Icon(
-                                  Icons.lock,
-                                  color: Theme.of(context).primaryColorLight,
-                                ),
-                                errorText: snapshot.data?.isEmpty == true
-                                    ? null
-                                    : snapshot.data,
-                              ),
-                              obscureText: true,
-                              onChanged: presenter.validatePassword,
-                            );
-                          }),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CircularProgressIndicator(),
+                        SizedBox(height: 10),
+                        Text(
+                          'Aguarde...',
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
                     ),
-                    StreamBuilder<bool>(
-                        stream: presenter.isFormValidStream,
-                        builder: (context, snapshot) {
-                          return ElevatedButton(
-                            onPressed:
-                                snapshot.data == true ? presenter.auth : null,
-                            child: Text(
-                              'Entrar'.toUpperCase(),
-                            ),
-                            style: ButtonStyle(),
-                          );
-                        }),
-                    TextButton.icon(
-                        onPressed: () {},
-                        icon: Icon(Icons.person),
-                        label: Text('Criar conta'))
                   ],
                 ),
-              ),
+              );
+            }
+          });
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                LoginHeader(),
+                Headline1(
+                  text: 'login',
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Form(
+                    child: Column(
+                      children: [
+                        StreamBuilder<String>(
+                            stream: presenter.emailErrorStream,
+                            builder: (context, snapshot) {
+                              return TextFormField(
+                                decoration: InputDecoration(
+                                  labelText: 'Email',
+                                  icon: Icon(
+                                    Icons.email,
+                                    color: Theme.of(context).primaryColorLight,
+                                  ),
+                                  errorText: snapshot.data?.isEmpty == true
+                                      ? null
+                                      : snapshot.data,
+                                ),
+                                keyboardType: TextInputType.emailAddress,
+                                onChanged: presenter.validateEmail,
+                              );
+                            }),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 8.0,
+                            bottom: 32,
+                          ),
+                          child: StreamBuilder<String>(
+                              stream: presenter.passwordErrorStream,
+                              builder: (context, snapshot) {
+                                return TextFormField(
+                                  decoration: InputDecoration(
+                                    labelText: 'Senha',
+                                    icon: Icon(
+                                      Icons.lock,
+                                      color:
+                                          Theme.of(context).primaryColorLight,
+                                    ),
+                                    errorText: snapshot.data?.isEmpty == true
+                                        ? null
+                                        : snapshot.data,
+                                  ),
+                                  obscureText: true,
+                                  onChanged: presenter.validatePassword,
+                                );
+                              }),
+                        ),
+                        StreamBuilder<bool>(
+                            stream: presenter.isFormValidStream,
+                            builder: (context, snapshot) {
+                              return ElevatedButton(
+                                onPressed: snapshot.data == true
+                                    ? presenter.auth
+                                    : null,
+                                child: Text(
+                                  'Entrar'.toUpperCase(),
+                                ),
+                                style: ButtonStyle(),
+                              );
+                            }),
+                        TextButton.icon(
+                            onPressed: () {},
+                            icon: Icon(Icons.person),
+                            label: Text('Criar conta'))
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
